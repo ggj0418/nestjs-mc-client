5e66cce3b84c7cb793f4f1320ce2713ad2f545f4
Kahnco <44552228+ggj0418@users.noreply.github.com>
2023-03-02 16:36:51 +0900

    delete unnecessary controller

--- src/acco-svc/acco-svc.module.ts
+++ src/acco-svc/acco-svc.module.ts
@@ -1,10 +1,8 @@
 import { Global, Module } from '@nestjs/common';
 import { AccoSvcService } from './services/acco-svc.service';
-import { AccoSvcController } from './controllers/acco-svc.controller';
 
 @Global()
 @Module({
-  controllers: [AccoSvcController],
   providers: [AccoSvcService],
   exports: [AccoSvcService],
 })
