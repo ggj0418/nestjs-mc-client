7f2dd35bffea2c3627a3c459e7b9cd79aa6ef2f7
Kahnco <44552228+ggj0418@users.noreply.github.com>
2023-03-02 16:27:18 +0900

    delete unnecessary controller

--- src/acco-svc/controllers/acco-svc.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Logger } from '@nestjs/common';
-import { AccoSvcService } from '../services/acco-svc.service';
-
-@Controller('acco-svc')
-export class AccoSvcController {
-  private logger = new Logger(AccoSvcController.name);
-
-  constructor(private readonly accoSvcService: AccoSvcService) {}
-}
